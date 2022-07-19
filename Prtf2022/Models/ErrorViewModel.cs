741e7c1099f4038a69a0e30f0e9af9528e2a47c2
fr48282 <fr48292@ubt-uni.net>
2022-07-19 21:08:46 +0200

    files

---
+++ Prtf2022/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prtf2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
